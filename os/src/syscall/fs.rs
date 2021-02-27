aa6db6852d1f622aa48e607ae2a289018caf5285
陈张萌 <2017013678@secoder.net>
2021-02-27 23:07:20 +0800

    [lab2] save comments

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,5 +1,11 @@
 const FD_STDOUT: usize = 1;
 
+/// 功能：将内存中缓冲区中的数据写入文件。
+/// 参数：`fd` 表示待写入文件的文件描述符；
+///      `buf` 表示内存中缓冲区的起始地址；
+///      `len` 表示内存中缓冲区的长度。
+/// 返回值：返回成功写入的长度。
+/// syscall ID：64
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
