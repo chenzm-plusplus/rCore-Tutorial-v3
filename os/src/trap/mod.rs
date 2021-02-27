f25b51ae52c9ab7c3fe42168c68ed5600d7e1bd3
陈张萌 <2017013678@secoder.net>
2021-02-28 00:27:02 +0800

    [lab2] comment

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -22,6 +22,7 @@ pub fn init() {
     }
 }
 
+//留下的问题：trap——handler传入的参数只有寄存器们。怎么判断是不是有超出内存区域？
 #[no_mangle]
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     let scause = scause::read();
@@ -29,6 +30,13 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
+            //暂且只在这里执行了syscall
+            //ID，args
+            //我觉得要不就在这里判断是否可以执行好了······
+            //
+            /// 参数：`fd` 表示待写入文件的文件描述符；
+    ///      `buf` 表示内存中缓冲区的起始地址；
+    ///      `len` 表示内存中缓冲区的长度。
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
         Trap::Exception(Exception::StoreFault) |
