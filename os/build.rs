6f8ddaeef0c7bfeaa658b444ce78ec80231a9169
陈张萌 <2017013678@secoder.net>
2021-03-01 16:56:06 +0800

    [lab3] fix test dir path

--- os/build.rs
+++ os/build.rs
@@ -7,11 +7,15 @@ fn main() {
     insert_app_data().unwrap();
 }
 
-static TARGET_PATH: &str = "../user/target/riscv64gc-unknown-none-elf/release/";
+// static TARGET_PATH: &str = "../user/target/riscv64gc-unknown-none-elf/release/";
+static TARGET_PATH: &str = "../../rCore_tutorial_tests/user/build/bin/";
+
+//"../../rCore_tutorial_tests/user/build/bin/"
 
 fn insert_app_data() -> Result<()> {
     let mut f = File::create("src/link_app.S").unwrap();
-    let mut apps: Vec<_> = read_dir("../user/src/bin")
+    // let mut apps: Vec<_> = read_dir("../user/src/bin")
+    let mut apps: Vec<_> = read_dir("../../rCore_tutorial_tests/user/build/bin/")
         .unwrap()
         .into_iter()
         .map(|dir_entry| {
