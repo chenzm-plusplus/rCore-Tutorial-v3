4b1e90e629f48af6e461a5b86fc9d5cdc8dc0144
陈张萌 <2017013678@secoder.net>
2021-03-04 15:00:43 +0800

    [lab1] report finish #2

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/user_lib-0b2700cc2dd96485.d
@@ -0,0 +1,10 @@
+/Users/chenzm/Projects/Rust/OS/rCore-Tutorial-v3/user/target/riscv64gc-unknown-none-elf/release/deps/user_lib-0b2700cc2dd96485.rmeta: src/lib.rs src/console.rs src/syscall.rs src/lang_items.rs
+
+/Users/chenzm/Projects/Rust/OS/rCore-Tutorial-v3/user/target/riscv64gc-unknown-none-elf/release/deps/libuser_lib-0b2700cc2dd96485.rlib: src/lib.rs src/console.rs src/syscall.rs src/lang_items.rs
+
+/Users/chenzm/Projects/Rust/OS/rCore-Tutorial-v3/user/target/riscv64gc-unknown-none-elf/release/deps/user_lib-0b2700cc2dd96485.d: src/lib.rs src/console.rs src/syscall.rs src/lang_items.rs
+
+src/lib.rs:
+src/console.rs:
+src/syscall.rs:
+src/lang_items.rs:
