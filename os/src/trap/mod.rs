8f9c513ed32eb98d455391c88ddbd230733c16e5
陈张萌 <2017013678@secoder.net>
2021-03-08 14:18:24 +0800

    [lab2] remove debug print #3

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -32,21 +32,7 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         Trap::Exception(Exception::UserEnvCall) => {
             trace!("trap_handler::Exception::UserEnvCall");
             cx.sepc += 4;
-            //暂且只在这里执行了syscall
-            //ID，args
-            //我觉得要不就在这里判断是否可以执行好了······
-            //
-            // 参数：`fd` 表示待写入文件的文件描述符；
-            //      `buf` 表示内存中缓冲区的起始地址；
-            //      `len` 表示内存中缓冲区的长度。
-            
-            //判断有没有超过···如果超过了就执行，否则?
-            // trace!("fd:{},buf:{:#x},len:{}",cx.x[10],cx.x[11],cx.x[12]);
-            // let (left,right) = address_space_current();
-            // if left<=cx.x[11] && right>cx.x[11]+cx.x[12]{
-            debug!("syscall...");
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
-            // }
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
