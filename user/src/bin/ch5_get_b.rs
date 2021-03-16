83f90f140a7c153265f23b47a7732ce00cb76f5a
陈张萌 <2017013678@secoder.net>
2021-03-16 20:35:13 +0800

    [lab5] report #5

--- user/src/bin/ch5_get_b.rs
+++ user/src/bin/ch5_get_b.rs
@@ -20,7 +20,7 @@ fn get_b() -> i32{
 pub fn main() -> i32{
     let a = get_a();
     //怎么做啊？如果用spawn就会直接死循环诶···
-    if spawn("ch5_test2\0") != 0 {
+    if fork() == 0 {
         let b = get_b();
         println!("a + b = {}", a + b);
         exit(0);
